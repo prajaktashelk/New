320c37aedff47ce68ee3ba27740a26b30c55d2c5
prajaktashelk <115137349+prajaktashelk@users.noreply.github.com>
2023-01-13 21:52:16 +0530

    Add files via upload
    spring

---
+++ GroceriesProject/src/main/java/com/example/GroceriesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceriesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceriesProjectApplication.class, args);
+	}
+
+}
